3b19a73c04415c89295f7c8c1729aabe216f2f0e
Lennaert Meijvogel <l.meijvogel@websend.nl>
2016-05-20 08:56:11 +0200

    Add aliases for 'rake db:migrate'

--- aliases.sh
+++ aliases.sh
@@ -5,6 +5,8 @@ alias rdfl='bundle exec rake db:fixtures:load'
 alias rdsl='bundle exec rake db:schema:load'
 alias trdsl='RAILS_ENV=test bundle exec rake db:schema:load'
 alias rdtp='bundle exec rake db:test:prepare'
+alias rmig='bundle exec rake db:migrate'
+alias rmigt='RAILS_ENV=test bundle exec rake db:migrate'
 
 alias orig='find . -regex "\\(.*\\(BACKUP\\|BASE\\|LOCAL\\|REMOTE\\).*\\|.*\\.orig\\).*" -delete'
 
