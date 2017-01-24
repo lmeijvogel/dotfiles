635d54ebb1fe86f4e0bfda0f4856e9333e5a57ae
Lennaert Meijvogel <l.meijvogel@yahoo.co.uk>
2017-01-24 09:03:09 +0100

    Remove original commented-out source from initialize_config_repo.rb

--- scripts/initialize_config_repo.rb
+++ scripts/initialize_config_repo.rb
@@ -42,38 +42,8 @@ FileUtils.mkdir_p bundle_path
 `cd #{bundle_path} ; git clone https://github.com/gmarik/Vundle.vim.git`
 `cd #{bundle_path} ; git clone https://github.com/olivierverdier/zsh-git-prompt $HOME/.zsh-git-prompt`
 `cd #{bundle_path} ; git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh`
-__END__
-for file in `ls -A ../ | grep -vP "^(bin|scripts|.git|.gitignore|README|INSTALL.md)$"`; do
-  CURRENT_TARGET_PATH=$(readlink -f "../$file")
-
-  export file="$HOME/.$file"
-  # Remove any file that was previously there
-  rm_if_link $file
-
-  if [ -e "$file" ]; then
-    rm -i "$file"
-  fi
-
-  # Create a softlink in the homedir to the version controlled file/dir
-  ln -s "$CURRENT_TARGET_PATH" "$file"
-done
-
-mkdir -p $HOME/bin
-
-for file in `ls -A ../bin/`; do
-  CURRENT_TARGET_PATH=$(readlink -f "../bin/$file")
-
-  export file="$HOME/bin/$file"
-  # Remove any file that was previously there
-  rm_if_link $file
-
-  if [ -e "$file" ]; then
-    rm -i "$file"
-  fi
-
-  # Create a softlink in the homedir to the version controlled file/dir
-  ln -s "$CURRENT_TARGET_PATH" "$file"
-done
 
 
-popd
+
+
+
