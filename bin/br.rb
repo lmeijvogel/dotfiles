a0f7998d1268733fafa63a08a27a356c5e437636
Lennaert Meijvogel <l.meijvogel@websend.nl>
2017-02-27 16:47:50 +0100

    Make handling of git-br outside of a git repo a bit cleaner.

--- bin/br.rb
+++ bin/br.rb
@@ -6,6 +6,8 @@ $LOAD_PATH << File.dirname(__FILE__)
 
 require '_create_shell_variables'
 
+NoGitRepo = Class.new(StandardError)
+
 class NumberedGitBranch
   def initialize(local_or_remote)
     @local_or_remote = local_or_remote
@@ -15,6 +17,8 @@ class NumberedGitBranch
     shell_variables = CreateShellVariables.new
 
     shell_variables.perform(branches)
+  rescue NoGitRepo
+    # Error was already printed
   end
 
   def branches
@@ -35,6 +39,8 @@ class NumberedGitBranch
 
   def get_branch_names(prefix)
     all_refs = `git for-each-ref --format '%(refname)'`.each_line.map(&:strip)
+
+    raise NoGitRepo unless $?.success?
 
     all_refs.select { |ref| ref =~ prefix }.map { |ref| ref.gsub(prefix, '') }
   end
