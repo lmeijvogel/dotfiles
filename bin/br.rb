d25a5d70e291b108b9a02427f8d5a794bda89065
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2018-06-11 22:09:24 +0200

    Use an MRU list for git-br local branches.

--- bin/br.rb
+++ bin/br.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-require 'open3'
+require "open3"
 
 $LOAD_PATH << File.dirname(__FILE__)
 
-require '_create_shell_variables'
+require "_create_shell_variables"
 
 NoGitRepo = Class.new(StandardError)
 
@@ -25,7 +25,7 @@ class NumberedGitBranch
   end
 
   def local_branches
-    get_branch_names(%r[^refs/heads/])
+    BranchMRU.new.branch_mru
   end
 
   def remote_branches
@@ -37,12 +37,12 @@ class NumberedGitBranch
 
     raise NoGitRepo unless $?.success?
 
-    all_refs.select { |ref| ref =~ prefix }.map { |ref| ref.gsub(prefix, '') }
+    all_refs.select { |ref| ref =~ prefix }.map { |ref| ref.gsub(prefix, "") }
   end
 end
 
 def forward_to_normal_git_branch!(argv)
-  stdout_or_stderr, = Open3.capture2e('git', 'branch', *argv)
+  stdout_or_stderr, = Open3.capture2e("git", "branch", *argv)
 
   $stderr.puts stdout_or_stderr
 end
@@ -75,6 +75,78 @@ def show_branch_choice(local_or_remote)
     end
   end
 end
+##########################################
+require 'shellwords'
+
+class ReflogEntry
+  attr_accessor :sha, :ref, :action, :message
+
+  def initialize(sha, ref, action, message)
+    self.sha = sha
+    self.ref = ref
+    self.action = action
+    self.message = message
+  end
+
+  def to_s
+    "#{sha} #{ref} #{action} #{message}"
+  end
+end
+
+class BranchMRU
+  def initialize
+    @branch_existence_registry = {}
+  end
+
+  def branch_mru
+    sorted_branch_names.select do |branch_name|
+      branch_exists?(branch_name)
+    end
+  end
+
+  private
+
+  def sorted_branch_names
+    branch_names = checkouts.map do |checkout|
+      matches = checkout.message.match(%r{moving from (?:[0-9a-zA-Z\-/]+) to ([0-9a-zA-Z\-/]+)})
+
+      matches[1]
+    end.uniq
+
+    branch_names.sort do |one, two|
+      if one == "master"
+        -1
+      elsif two == "master"
+        1
+      else
+        0
+      end
+    end
+  end
+
+  def checkouts
+    logs = `git reflog`.split("\n")
+
+    reflog_entries = logs.map do |line|
+      match = line.match(/([0-9a-f]+)\s+([^:]+):\s+([^:]+):\s+(.*)/)
+
+      ReflogEntry.new(*match[1..-1])
+    end
+
+    reflog_entries.select { |entry| entry.action == "checkout" }
+  end
+
+  def branch_exists?(branch_name)
+    if @branch_existence_registry.key?(branch_name)
+      return @branch_existence_registry[branch_name]
+    end
+
+    `git rev-parse --verify refs/heads/#{Shellwords.shellescape(branch_name)} 2>/dev/null`
+
+    @branch_existence_registry[branch_name] = $?.success?
+  end
+end
+
 
 begin
   if (ARGV - ['--remote', '-r']).empty?
