a6c47ab8b8a328e18c52a6fb5f2a10cc350e59db
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2018-07-26 16:53:22 +0200

    Make screensaver screenshot monochrome.

--- lib/lock-screen.sh
+++ lib/lock-screen.sh
@@ -4,7 +4,7 @@
 scrot /tmp/screen_locked.png
 
 # Pixellate it 10x
-mogrify -scale 8% -scale 1250% /tmp/screen_locked.png
+mogrify -monochrome -scale 8% -scale 1250% /tmp/screen_locked.png
 
 # Lock screen displaying this image.
 i3lock -i /tmp/screen_locked.png
