58e6c810a67a8a2e8cbc2669dd9fc75358a2937d
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2018-07-30 17:04:10 +0200

    Replace locking script with a simpler (faster!) one that shows a static image.

--- lib/lock-screen.sh
+++ lib/lock-screen.sh
@@ -1,13 +1,19 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 # Take a screenshot
-scrot /tmp/screen_locked.png
+# scrot /tmp/screen_locked.png
 
 # Pixellate it 10x
-mogrify -monochrome -scale 8% -scale 1250% /tmp/screen_locked.png
+# mogrify -monochrome -scale 8% -scale 1250% /tmp/screen_locked.png
+
+PICTURE_FILE=$HOME/Pictures/lockscreen-background.png
 
 # Lock screen displaying this image.
-i3lock -i /tmp/screen_locked.png
+if [[ -f $PICTURE_FILE ]]; then
+  i3lock -t -c 000000 -i $PICTURE_FILE
+else
+  i3lock -c 000000
+fi
 
 # Turn the screen off after a delay.
 sleep 60; pgrep i3lock && xset dpms force off
