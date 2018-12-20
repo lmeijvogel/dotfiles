15fea8cdfa002caf965cecb9656ba8f9a773abdc
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2018-12-20 20:34:24 +0100

    Add all elpa plugins for emacs to the repo.

---
+++ emacs.d/elpa/dash-20180910.1856/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (23572 7186 44642 588000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
---
+++ emacs.d/elpa/ztree-1.0.5/ztree-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from ztree.el
+(define-package "ztree" "1.0.5" "Text mode directory tree" '((cl-lib "0")) :url "https://github.com/fourier/ztree" :keywords '("files" "tools"))
