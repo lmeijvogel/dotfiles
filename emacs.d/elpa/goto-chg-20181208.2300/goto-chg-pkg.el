b262efd165f21b5507b79b627e139a1e5ec195c5
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-01-14 14:38:45 +0100

    Upgrade emacs packages.

--- emacs.d/elpa/goto-chg-20181208.2300/goto-chg-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "goto-chg" "20181208.2300" "goto last change" 'nil :commit "c664d8787939dfdf060d0b4cfe1240aeee62ffc8" :keywords '("convenience" "matching") :authors '(("David Andersson <l.david.andersson(at)sverige.nu>")) :maintainer '("Vasilij Schneidermann" . "v.schneidermann@github.com") :url "https://github.com/emacs-evil/goto-chg")
---
+++ emacs.d/elpa/ztree-20180512.1850/ztree-pkg.el
@@ -0,0 +1,12 @@
+(define-package "ztree" "20180512.1850" "Text mode directory tree"
+  '((cl-lib "0"))
+  :keywords
+  '("files" "tools")
+  :authors
+  '(("Alexey Veretennikov" . "alexey.veretennikov@gmail.com"))
+  :maintainer
+  '("Alexey Veretennikov" . "alexey.veretennikov@gmail.com")
+  :url "https://github.com/fourier/ztree")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
