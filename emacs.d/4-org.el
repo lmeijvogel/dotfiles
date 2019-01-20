75d9e2f0c76be7049ad2a6525bc61eacd9aa2618
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-01-20 22:15:27 +0100

    Add some org mode configuration.

--- emacs.d/4-org.el
+++ emacs.d/4-org.el
@@ -1,4 +1,8 @@
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-switchb)
+;; This is already the default location, but repeating it
+(setq org-directory "~/org")
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c b") 'org-switchb)
