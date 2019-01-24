1e1006616fc28a4939c19221e158dae8cbe12c73
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-01-24 16:17:46 +0100

    Replace emacs lsp-mode with tide again.
    lsp-mode had a few problems for typescript development:
    the recommended language servers did not handle e.g. adding imports
    well. Hopefully tide performs better.

--- emacs.d/3-plugins.el
+++ emacs.d/3-plugins.el
@@ -92,7 +92,10 @@
   :after flycheck
   :config
   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-  (add-hook 'web-mode-hook)
+  (add-hook 'web-mode-hook
+       (lambda ()
+         (when (string-equal "tsx" (file-name-extension buffer-file-name))
+           (setup-tide-mode))))
   ;; enable typescript-tslint checker
   (flycheck-add-mode 'typescript-tslint 'web-mode)
   ;; No quotes after html tag props.
