a58c91d2e40c76167e6c700fdb1a190f18df5bb1
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-01-25 15:55:51 +0100

    Add org agenda files and and additional todo keywords.

--- emacs.d/4-org.el
+++ emacs.d/4-org.el
@@ -2,6 +2,8 @@
 (setq org-directory "~/org")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 
+(setq org-todo-keywords '((sequence "TODO" "STARTED" "WAITING" "|" "DONE" "CANCELLED")))
+"
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
