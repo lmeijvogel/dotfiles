d428870ff12493dc5b371b924a8d2e5e0b09efc5
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-01-28 14:28:05 +0100

    Allow running ruby code from org files.

--- emacs.d/4-org.el
+++ emacs.d/4-org.el
@@ -2,7 +2,7 @@
   :ensure t
   :config
 
-  ;; This is already the default location, but repeating it
+  ;; This is already the default location, but repeating it doesn't hurt
   (setq org-directory "~/org")
   (setq org-default-notes-file (concat org-directory "/notes.org"))
 
@@ -12,4 +12,11 @@
   (global-set-key (kbd "C-c a") 'org-agenda)
   (global-set-key (kbd "C-c c") 'org-capture)
   (global-set-key (kbd "C-c b") 'org-switchb)
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '(
+     (emacs-lisp . nil)
+     (ruby . t))
+   )
 )
