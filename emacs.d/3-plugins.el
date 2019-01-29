fd9bc12b9fe203727789e9a668bb8860e92d0aa6
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-01-29 16:56:59 +0100

    Remove doom-themes.

--- emacs.d/3-plugins.el
+++ emacs.d/3-plugins.el
@@ -144,28 +144,6 @@
     'treemacs)
 )
 
-(use-package doom-themes
-  :after all-the-icons
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-    doom-themes-enable-italic t) ; if nil, italics is universally disabled
-
-  ;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
-  ;; may have their own settings.
-  (load-theme 'doom-one t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-
-  ;; or for treemacs users
-  (doom-themes-treemacs-config)
-
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config)
-)
-
 (use-package tabbar
   :ensure t
   :config
