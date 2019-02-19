aa7156385f515a4b86d24fa34066f852f9282234
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-02-19 16:58:39 +0100

    Upgrade emacs packages.

--- emacs.d/elpa/swiper-20190206.1951/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20190206.1951" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.11.0")) :commit "3216d4060b9614428c1fcf87f324ca83c97d6d24" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
