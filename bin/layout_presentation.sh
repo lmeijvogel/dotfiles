952a1de93b7c32e6ea5303710178cdf1db2c3efe
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-03-04 09:10:21 +0100

    Add i3wm shortcut keys for changing screen layouts.

---
+++ bin/layout_presentation.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1-2 --off --output eDP-1-1 --mode 1920x1080 --pos 1920x800 --rotate normal --output HDMI-1-1 --primary --mode 1920x1080 --pos 0x800 --rotate normal --output DP-1-1 --off --output DP-1-2 --off
