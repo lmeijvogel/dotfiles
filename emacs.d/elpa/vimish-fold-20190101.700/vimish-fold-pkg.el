9e73931d80d483cd9c8e0f925d62a359d3b7efc8
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-03-11 08:59:24 +0100

    Add vim-style code folding to Emacs.

---
+++ emacs.d/elpa/vimish-fold-20190101.700/vimish-fold-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vimish-fold" "20190101.700" "Fold text like in Vim" '((emacs "24.4") (cl-lib "0.5") (f "0.18.0")) :commit "6ddc79fec2833e45f77df2286ba2aee200049572" :keywords '("convenience") :authors '(("Mark Karpov" . "markkarpov92@gmail.com")) :maintainer '("Mark Karpov" . "markkarpov92@gmail.com") :url "https://github.com/mrkkrp/vimish-fold")
