b808e2a96a02936dc9357296a3ee898ceadab0c2
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-03-12 16:52:28 +0100

    Upgrade emacs packages.

---
+++ emacs.d/elpa/counsel-projectile-20190310.2205/counsel-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-projectile" "20190310.2205" "Ivy integration for Projectile" '((counsel "0.11.0") (projectile "2.0.0")) :commit "edc60cd4297ba9c746332ad833a36e51d54c29bf" :keywords '("project" "convenience") :authors '(("Eric Danan")) :maintainer '("Eric Danan") :url "https://github.com/ericdanan/counsel-projectile")
---
+++ emacs.d/elpa/treemacs-projectile-20190309.1812/treemacs-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-projectile" "20190309.1812" "Projectile integration for treemacs" '((projectile "0.14.0") (treemacs "0.0")) :commit "bd3e563323bda721d69cd3353b32fdcb4710ffe4" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
