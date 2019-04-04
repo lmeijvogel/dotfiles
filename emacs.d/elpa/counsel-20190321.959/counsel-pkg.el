0795dc59251b671d473767ee0591de9042ac9a24
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-04-04 09:01:08 +0200

    Upgrade emacs packages.

--- emacs.d/elpa/counsel-20190321.959/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/lennaert/.emacs.d/elpa/counsel-20190321.959/counsel.el  -*- no-byte-compile: t -*-
-(define-package "counsel" "20190321.959" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.11.0")) :commit "e7146e58f94b1c519635596944f008f3ba28b546" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
--- emacs.d/elpa/treemacs-projectile-20190313.626/treemacs-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/lennaert/.emacs.d/elpa/treemacs-projectile-20190313.626/treemacs-projectile.el  -*- no-byte-compile: t -*-
-(define-package "treemacs-projectile" "20190313.626" "Projectile integration for treemacs" '((projectile "0.14.0") (treemacs "0.0")) :commit "920ec3618d345586e0f350bd15960826a6fdb73d" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
