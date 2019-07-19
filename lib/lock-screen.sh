751a9fd41a539d33cd1eeaf00f0757894c022e32
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2019-07-19 16:52:48 +0200

    Remove logging from screen lock script.

--- lib/lock-screen.sh
+++ lib/lock-screen.sh
@@ -1,32 +1,26 @@
 #!/bin/bash -e
 
 schedule_dpms() {
-  echo "Scheduling dpms" >>/tmp/zee_log
   sleep 60 # 1 minute
-  echo "dpms: Done sleeping" >>/tmp/zee_log
-  pgrep i3lock && echo "i3lock found: dpms" >>/tmp/zee_log
+  pgrep i3lock
   pgrep i3lock && start_dpms
   # start_dpms
 }
 
 schedule_suspend() {
-  echo "Scheduling suspend" >>/tmp/zee_log
   sleep 900 # 15 minutes
-  echo "Suspend: Done sleeping" >>/tmp/zee_log
-  pgrep i3lock >>/tmp/zee_log 2>&1
-  pgrep i3lock && echo "i3lock found: suspending" >>/tmp/zee_log
+  pgrep i3lock
+  pgrep i3lock
   pgrep i3lock && start_suspend
   # start_suspend
 }
 
 start_dpms() {
-  echo "Starting DPMS" >>/tmp/zee_log
-  xset dpms force off >>/tmp/zee_log 2>&1
+  xset dpms force off
 }
 
 start_suspend() {
-  echo "Starting suspend" >>/tmp/zee_log 2>&1
-  systemctl suspend >>/tmp/zee_log 2>&1
+  systemctl suspend
 }
 
 # Take a screenshot
@@ -44,7 +38,6 @@ else
   i3lock -c 000000
 fi
 
-echo "Script start" >/tmp/zee_log
 # Turn the screen off after a delay.
 
 schedule_dpms&
