617c30e417a00054d7f41da7b79ee702d499b4b1
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2019-09-27 08:57:58 +0200

    Remove older layout commands.

--- bin/layout_desk_work.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-1-2 --off --output eDP-1-1 --mode 1920x1080 --pos 5120x880 --rotate normal --output HDMI-1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1-1 --off --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate normal
