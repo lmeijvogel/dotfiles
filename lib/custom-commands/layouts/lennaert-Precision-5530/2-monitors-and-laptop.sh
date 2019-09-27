f6b8b6704d9f4ae3f84f778b4e009250f0d3e1a9
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2019-09-27 08:56:29 +0200

    Add monitor layouts for work.

---
+++ lib/custom-commands/layouts/lennaert-Precision-5530/2-monitors-and-laptop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off --output DP-1-3 --off --output eDP-1 --mode 2048x1152 --pos 5120x1080 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-3 --off --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal
---
+++ lib/custom-commands/layouts/lennaert-Precision-5530/presentation.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off --output DP-1-3 --off --output eDP-1 --mode 2048x1152 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-3 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-2 --off --output DP-1-1 --off
