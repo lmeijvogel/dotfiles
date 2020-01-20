8fbf91a0ca547a167378834859cdec7528ab93b0
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2020-01-20 14:53:56 +0100

    Change display defaults after monitor renumbering.

--- lib/custom-commands/layouts/lennaert-Precision-5530/2-monitors.sh
+++ lib/custom-commands/layouts/lennaert-Precision-5530/2-monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off --output DP-1-3 --off --output eDP-1 --off --output DP-2 --off --output DP-1 --off --output DP-3 --off --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal
+xrandr --output DP-1-2-8 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1-3 --off --output DP-1-2-1 --off --output eDP-1 --off --output DP-1-2 --off --output DP-1 --off --output DP-3 --off --output DP-2 --off --output DP-1-1 --mode 2560x1440 --pos 0x0 --rotate normal
